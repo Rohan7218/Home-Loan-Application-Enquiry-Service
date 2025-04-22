67522fef6ca81150506ab48c78c510dbd2ca6233
Aashish71-stack <aashishghule52@gmail.com>
2025-04-22 11:50:06 +0530

    all changes saved

--- src/main/java/com/example/enquiry/rest/EnquiryController.java
+++ src/main/java/com/example/enquiry/rest/EnquiryController.java
@@ -1,12 +1,10 @@
 package com.example.enquiry.rest;
 
 import javax.validation.Valid;
-import javax.websocket.server.PathParam;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
